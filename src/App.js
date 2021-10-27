a35bfbb88575f0f584d5dc3a8bad332607d86cc1
Jessica Wray <jessica.wray@gmail.com>
2021-10-27 16:49:00 -0700

    add the 🔥

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Puffer from './Puffer.js';
+
+const numPuffers = 8
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="puffers-container">
+        {[...Array(numPuffers)].map( ( _num, i ) => {
+          return (
+            <div className="puffer-wrapper">
+              <Puffer key={`puffer-${i}`} />
+            </div> 
+          );
+        })}
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Puffer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Puffer = () => <div>🔥</div>;
+
+export default Puffer;
+
